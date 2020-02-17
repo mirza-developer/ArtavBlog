a1eda4a030aba80809dbf8d0672ea942841af87a
artav <artav.work@outlook.com>
2020-02-16 18:00:09 -0800

    981127

--- ArtavBlog/Controllers/ManageController.cs
+++ ArtavBlog/Controllers/ManageController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ArtavBlog.Models.Base;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArtavBlog.Controllers
 {
     public class ManageController : Controller
     {
+        private BlogContext _context;
+        public ManageController(BlogContext context)
+        {
+            _context = context;
+        }
         public IActionResult CareChat()
         {
+            var availCareChats = _context.CareMessageDb.FromSqlRaw("Select Distinct CareMessage.ID, CareMessage.CreatorIdentityID,CareMessage.ConnectionId,(Select top(1) CareMessage.CreateDateAndTime Order by CareMessage.CreateDateAndTime DESC) as [CreateDateAndTime] from SignalR.CareMessage WHERE CareMessage.IsDeleted = 0").ToList();
             return View();
         }
     }
